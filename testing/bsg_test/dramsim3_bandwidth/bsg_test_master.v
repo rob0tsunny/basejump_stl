9d6bd8bd5cc65cc64e5b917129b99e516cecd91d
Tommy Jung <tommy.dc.jung@gmail.com>
2020-01-17 14:39:29 -0800

    codes

--- testing/bsg_test/dramsim3_bandwidth/bsg_test_master.v
+++ testing/bsg_test/dramsim3_bandwidth/bsg_test_master.v
@@ -1,14 +1,51 @@
 module bsg_test_master
-  #(
+  #(parameter channel_addr_width_p="inv"
+    , parameter num_request_p="inv"
   )
   (
     input clk_i
     , input reset_i
 
     , input v_i
-    , input write_not_read_i
-    , input addr_i
+    , input [channel_addr_width_p-1:0] ch_addr_i
+    , output logic yumi_o
+
+    , output logic dram_v_o
+    , output logic [channel_addr_width_p-1:0] dram_ch_addr_o
+    , input dram_yumi_i
+
+    , input dram_data_v_i
   );
+
+
+
+
+  // credit counter
+  logic credit_up;
+  logic credit_down;
+  logic [`BSG_WIDTH(max_val_p)-1:0] credit_lo;
+
+  bsg_counter_up_down #(
+    .max_val_p(num_request_p)
+    ,.init_val_p(num_request_p)
+  ) cc0 (
+    .clk_i(clk_i)
+    ,.reset_i(reset_i)
+    ,.up_i(credit_up)
+    ,.down_i(credit_down)
+    ,.count_o(credit_lo)
+  );
+
+
+  assign dram_ch_addr_o = ch_addr_i;
+
+
+  assign yumi_o = v_i & (credit_lo != '0) & dram_yumi_i;
+
+  assign dram_v_o = v_i & (credit_lo != '0);
+
+  assign credit_up = dram_data_v_i;
+  assign credit_down = dram_v_o & dram_yumi_i;
 
 
 endmodule 
--- testing/bsg_test/dramsim3_bandwidth/testbench.v
+++ testing/bsg_test/dramsim3_bandwidth/testbench.v
@@ -1,8 +1,14 @@
+`include "bsg_nonsynth_dramsim3.svh"
+
+
+`define dram_pkg bsg_nonsynth_dramsim3_hbm2_8gb_x128_pkg
+
 module testbench();
 
 
   bit clk;
   bit reset;
+
   bsg_nonsynth_clock_gen #(
     .cycle_time_p(100)
   ) cg0 (
@@ -19,8 +25,10 @@ module testbench();
   );
 
 
+  import `dram_pkg::*;
+
   // trace replay
-  localparam payload_width_p = 10;
+  localparam payload_width_p = `dram_pkg::channel_addr_width_p;
   localparam rom_addr_width_p = 16;
 
   logic tr_v_lo;
@@ -33,8 +41,8 @@ module testbench();
   logic tr_done_lo;
 
   bsg_trace_replay #(
-    .payload_width_p()
-    ,.rom_addr_width_p()
+    .payload_width_p(payload_width_p)
+    ,.rom_addr_width_p(rom_addr_width_p)
   ) tr0 (
     .clk_i(clk)
     ,.reset_i(reset)
@@ -75,7 +83,7 @@ module testbench();
 
   bsg_fifo_1r1w_small #(
     .width_p(payload_width_p)
-    ,.els_p(16)
+    ,.els_p(32)
   ) req_fifo0 (
     .clk_i(clk)
     ,.reset_i(reset)
@@ -89,43 +97,98 @@ module testbench();
     ,.yumi_i(fifo_yumi_li)
   );
 
+
   // requester
   //
+  logic dram_v_lo;
+  logic [channel_addr_width_p-1:0] dram_ch_addr_lo;
+  logic dram_yumi_li;
+  logic dram_data_v_li;
+
   bsg_test_master #(
+    .channel_addr_width_p(`dram_pkg::channel_addr_width_p)
+    ,.num_request_p(2)
   ) tm0 (
     .clk_i(clk)
     ,.reset_i(reset)
 
+    ,.v_i(fifo_v_lo)
+    ,.ch_addr_i(fifo_data_lo)
+    ,.yumi_o(fifo_yumi_li)
+
+    ,.dram_v_o(dram_v_lo)
+    ,.dram_ch_addr_o(dram_ch_addr_lo)
+    ,.dram_yumi_i(dram_yumi_li)
+
+    ,.dram_data_v_i(dram_data_v_li)
   );
+
 
   // dramsim3
   //
+  logic [num_channels_p-1:0] dramsim3_v_li;
+  logic [num_channels_p-1:0][channel_addr_width_p-1:0] dramsim3_ch_addr_li;
+  logic [num_channels_p-1:0] dramsim3_yumi_lo;
+
+  logic [num_channels_p-1:0] dramsim3_data_v_lo;
+
   bsg_nonsynth_dramsim3 #(
-    .channel_addr_width_p()
-    ,.data_width_p()
-    ,.num_channels_p()
-    ,.num_columns_p()
-    ,.address_mapping_p()
-    ,.size_in_bits_p()
-    ,.config_p()
+    .channel_addr_width_p(`dram_pkg::channel_addr_width_p)
+    ,.data_width_p(`dram_pkg::data_width_p)
+    ,.num_channels_p(`dram_pkg::num_channels_p)
+    ,.num_columns_p(`dram_pkg::num_columns_p)
+    ,.size_in_bits_p(`dram_pkg::size_in_bits_p)
+    ,.address_mapping_p(`dram_pkg::address_mapping_p)
+    ,.config_p(`dram_pkg::config_p)
+    ,.debug_p(1)
   ) DUT (
     .clk_i(clk)
     ,.reset_i(reset)
 
-    ,.v_i()
-    ,.write_not_read_i()
-    ,.ch_addr_i()
-    ,.yumi_o()
+    ,.v_i(dramsim3_v_li)
+    ,.write_not_read_i('0) // you can only read in this test.
+    ,.ch_addr_i(dramsim3_ch_addr_li)
+    ,.yumi_o(dramsim3_yumi_lo)
 
-    ,.data_v_i()
-    ,.data_i()
+    ,.data_v_i('0)
+    ,.data_i('0)
     ,.data_yumi_o()
 
-    ,.data_v_o()
+    ,.data_v_o(dramsim3_data_v_lo)
     ,.data_o()
   ); 
 
+  assign dramsim3_v_li[0] = dram_v_lo;
+  assign dramsim3_ch_addr_li[0] = dram_ch_addr_lo;
+  assign dram_yumi_li = dramsim3_yumi_lo[0];
+  assign dram_data_v_li = dramsim3_data_v_lo[0];
 
+  for (genvar i = 1; i < `dram_pkg::channel_addr_width_p; i++) begin
+    assign dramsim3_v_li[i] = 1'b0;
+    assign dramsim3_ch_addr_li[i] = '0;
+  end
+
+
+
+  // request tracker
+  integer sent_r;
+  integer recv_r;
+
+  always_ff @ (posedge clk) begin
+    if (reset) begin
+      sent_r <= 0;
+      recv_r <= 0;
+    end
+    else begin
+      if (tr_v_lo & tr_yumi_li) sent_r++;
+      if (dramsim3_data_v_lo) recv_r++;
+    end
+  end
+
+  initial begin
+    wait(tr_done_lo & (sent_r == recv_r));
+    $finish();
+  end
 
 
 endmodule
